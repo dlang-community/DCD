d631d81947844dde3467078af451ac9dc597cd8d
Germán L. Osella Massa <gosella@gmail.com>
2014-07-31 11:46:21 -0300

    Update build.bat to be in sync with build.sh
    WARNING: dcd-server fails to build using both -O and -inline with message
    "Internal error: backend\cgcod.c 1594" using DMD 2.066-b6 (same thing with
    DMD 2.065).
    
    Removing -O or -inline solve the problem.

--- build.bat
+++ build.bat
@@ -1,3 +1,48 @@
-dmd -wi client.d messages.d stupidlog.d msgpack-d/src/msgpack.d -Imsgpack-d/src -release -inline -O -ofdcd-client
+del /f containers\src\std\allocator.d
 
-dmd  actypes.d conversion/astconverter.d conversion/first.d conversion/second.d conversion/third.d autocomplete.d constants.d messages.d modulecache.d semantic.d server.d stupidlog.d string_interning.d dscanner/std/d/ast.d dscanner/std/d/entities.d dscanner/std/d/lexer.d dscanner/std/d/parser.d dscanner/std/lexer.d dscanner/std/allocator.d dscanner/formatter.d containers/src/memory/appender.d containers/src/memory/allocators.d containers/src/containers/dynamicarray.d containers/src/containers/ttree.d containers/src/containers/hashset.d containers/src/containers/unrolledlist.d containers/src/containers/internal/hash.d msgpack-d/src/msgpack.d -Icontainers/src -Imsgpack-d/src -Idscanner -wi -g -O -release -ofdcd-server
+dmd^
+ client.d^
+ messages.d^
+ stupidlog.d^
+ msgpack-d/src/msgpack.d^
+ -Imsgpack-d/src^
+ -release -inline -O -wi^
+ -ofdcd-client
+
+dmd^
+ actypes.d^
+ conversion/astconverter.d^
+ conversion/first.d^
+ conversion/second.d^
+ conversion/third.d^
+ autocomplete.d^
+ constants.d^
+ messages.d^
+ modulecache.d^
+ semantic.d^
+ server.d^
+ stupidlog.d^
+ string_interning.d^
+ libdparse/src/std/d/ast.d^
+ libdparse/src/std/d/entities.d^
+ libdparse/src/std/d/lexer.d^
+ libdparse/src/std/d/parser.d^
+ libdparse/src/std/lexer.d^
+ libdparse/src/std/allocator.d^
+ libdparse/src/std/d/formatter.d^
+ containers/src/memory/allocators.d^
+ containers/src/memory/appender.d^
+ containers/src/containers/dynamicarray.d^
+ containers/src/containers/ttree.d^
+ containers/src/containers/unrolledlist.d^
+ containers/src/containers/hashset.d^
+ containers/src/containers/internal/hash.d^
+ containers/src/containers/internal/node.d^
+ containers/src/containers/slist.d^
+ msgpack-d/src/msgpack.d^
+ -Icontainers/src^
+ -Imsgpack-d/src^
+ -Ilibdparse/src^
+ -wi -O -release^
+ -ofdcd-server
+
