b0bbefa48bee91aee61f020306b3ac5ef54fe919
Hackerpilot <briancschott@gmail.com>
2016-01-07 21:10:19 -0800

    Tests for doc comments

---
+++ tests/tc024/file.d
@@ -0,0 +1,30 @@
+/** documentation for g */
+int g; /// more documentation for g
+
+/// test
+alias cool = string; /// which comment is there?
+
+/// doc1
+int a, /// doc2
+	b, /** doc3 */ /**doc3.5*/
+	c; /// doc4
+
+/**
+ * stuff
+ */
+int d; /** what could go wrong? */
+
+/** abc */
+/** def */
+int e;
+
+unittest
+{
+	a;
+	b;
+	c;
+	d;
+	e;
+	g;
+	cool;
+}
