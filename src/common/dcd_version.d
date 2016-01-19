b3dc41f2f8ff1a56e6d8c16c62d9325e7e101e9b
Hackerpilot <briancschott@gmail.com>
2016-01-19 09:20:11 -0800

    Increase version number

--- src/common/dcd_version.d
+++ src/common/dcd_version.d
@@ -21,7 +21,7 @@ module common.dcd_version;
 /**
  * Human-readable version number
  */
-enum DCD_VERSION = "v0.7.5";
+enum DCD_VERSION = "v0.8.0-alpha1";
 
 version (Windows) {}
 else version (built_with_dub) {}
