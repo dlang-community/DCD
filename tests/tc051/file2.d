224ed55474bff6c992d8e60530e169dfa57d8b9e
Basile Burg <basile.b@gmx.com>
2017-04-09 06:01:07 +0200

    fixup #369, dot completion broken in bodies
    + handle templatized UDAs

---
+++ tests/tc051/file2.d
@@ -0,0 +1,1 @@
+struct UDA(T){int a,b;} struct Foo{int c; @UDA!(int)(0,1) void bar(){this.}}
--- tests/tc051/run.sh
+++ tests/tc051/run.sh
@@ -6,3 +6,6 @@ diff actual.txt expected.txt
 
 ../../bin/dcd-client $1 file1.d -c25 > actual1.txt
 diff actual1.txt expected1.txt
+
+../../bin/dcd-client $1 file2.d -c74 > actual2.txt
+diff actual2.txt expected2.txt
