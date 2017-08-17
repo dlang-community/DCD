22dffe146842b351ed996132e25c1e4cd8555b77
WebFreak001 <janju007@web.de>
2017-08-17 19:33:00 +0200

    Remove test from server split

--- tests/tc057/file.d
+++
@@ -1,8 +0,0 @@
-void doStuff(int a, int b) { return; }
-int getInt(int i, int[] b) { return i; }
-int b(int a) { return a; }
-void main(string[] args)
-{
-	doStuff(getInt(1, [b(3)]),);
-	auto x = 10, 20;
-}
--- tests/tc057/run.sh
+++
@@ -1,5 +0,0 @@
-set -e
-set -u
-
-../../bin/dcd-client $1 file.d -c161 > actual1.txt
-diff actual1.txt expected1.txt
