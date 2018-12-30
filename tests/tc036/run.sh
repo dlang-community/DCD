26d410d4763d366f2ab4e81c18f763868acfc4f1
WebFreak001 <gh@webfreak.org>
2018-12-30 02:53:31 +0100

    Fix file import test bleeding into other tests

--- tests/tc036/run.sh
+++ tests/tc036/run.sh
@@ -1,6 +1,9 @@
 set -e
 set -u
 
+../../bin/dcd-client $1 file.d -c 15 | sed s\""$(dirname "$(pwd)")"\"\" > actual3.txt
+diff actual3.txt expected3.txt
+
 ../../bin/dcd-client $1 -I bar.d
 
 ../../bin/dcd-client $1 file.d -c 15 | sed s\""$(dirname "$(pwd)")"\"\" > actual1.txt
@@ -8,3 +11,8 @@ diff actual1.txt expected1.txt
 
 ../../bin/dcd-client $1 file.d -c 40 | sed s\""$(dirname "$(pwd)")"\"\" > actual2.txt
 diff actual2.txt expected2.txt
+
+../../bin/dcd-client $1 -R bar.d
+
+../../bin/dcd-client $1 file.d -c 15 | sed s\""$(dirname "$(pwd)")"\"\" > actual3.txt
+diff actual3.txt expected3.txt
