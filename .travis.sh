15f71142ec15347a68654cba22f5ece7a48fef8c
Jan Jurzitza <gh@webfreak.org>
2020-07-22 07:43:18 +0200

    use d-test-utils & bump libdparse to 0.15.x
    Because this release fixes the private protection issue through new
    dsymbol (since 0.11.0) we also need to bump libdparse to at least 0.15.0
    so no older libdparse versions are supported.

--- .travis.sh
+++ .travis.sh
@@ -3,8 +3,16 @@
 set -e
 
 if [[ $BUILD == dub ]]; then
-    dub build --build=release --config=client
-    dub build --build=release --config=server
+    if [[ -n $LIBDPARSE_VERSION ]]; then
+        rdmd ./d-test-utils/test_with_package.d $LIBDPARSE_VERSION libdparse -- dub build --build=release --config=client
+        rdmd ./d-test-utils/test_with_package.d $LIBDPARSE_VERSION libdparse -- dub build --build=release --config=server
+    elif [[ -n $DSYMBOL_VERSION ]]; then
+        rdmd ./d-test-utils/test_with_package.d $DSYMBOL_VERSION dsymbol -- dub build --build=release --config=client
+        rdmd ./d-test-utils/test_with_package.d $DSYMBOL_VERSION dsymbol -- dub build --build=release --config=server
+    else
+        echo 'Cannot run test without LIBDPARSE_VERSION nor DSYMBOL_VERSION environment variable'
+        exit 1
+    fi
 elif [[ $DC == ldc2 ]]; then
     git submodule update --init --recursive
     make ldc -j2
