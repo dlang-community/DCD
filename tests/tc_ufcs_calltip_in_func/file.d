2457f70b8e914af8ba980b7907161bca32fae543
WebFreak001 <gh@webfreak.org>
2022-12-07 20:38:18 +0100

    use `isCallableWithArg` in paren completion
    For consistency and to allow future changes to type matching (e.g.
    implicit type conversion)

--- tests/tc_ufcs_calltip_in_func/file.d
+++ tests/tc_ufcs_calltip_in_func/file.d
@@ -1,5 +1,9 @@
 void showSomething(int x, string message, bool ok){}
 void showSomething(int x, string message, bool ok, float percentage){}
+void showSomething(string x, string message, bool ok){}
+void showSomething(){}
+int showSomething;
+struct showSomething { int x; }
 
 void main(){
 	int x;
--- tests/tc_ufcs_calltip_in_func/run.sh
+++ tests/tc_ufcs_calltip_in_func/run.sh
@@ -1,5 +1,5 @@
 set -e
 set -u
 
-../../bin/dcd-client $1 -c163 file.d > actual.txt
+../../bin/dcd-client $1 -c293 file.d > actual.txt
 diff actual.txt expected.txt
