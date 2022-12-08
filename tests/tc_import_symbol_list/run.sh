88655ee049b9a9d9a2b1ef663c3d44cde4da643b
Chris <chris@hopfen.space>
2022-12-08 12:23:50 +0100

    Replaced 'stat' by 'wc -c' to make it work on MacOS too

--- tests/tc_import_symbol_list/run.sh
+++ tests/tc_import_symbol_list/run.sh
@@ -1,6 +1,6 @@
 set -e
 set -u
 
-../../bin/dcd-client $1 file.d --extended -I"$PWD"/newpackage -c$(stat -c %s file.d) > actual1.txt
+../../bin/dcd-client $1 file.d --extended -I"$PWD"/newpackage -c$(wc -c < file.d) > actual1.txt
 echo -e "identifiers\nSomeStruct\ts\t\t$PWD/newpackage/newmodule.d 26\t" > expected1.txt
 diff actual1.txt expected1.txt --strip-trailing-cr
