5d3b6b08ed09e93f1259671e0d7f35353ce48d64
davu <davu@mir-robots.com>
2023-02-22 00:19:34 +0100

    added alias this test

--- tests/tc_ufcs_struct_completion/fooutils/fooutils.d
+++ tests/tc_ufcs_struct_completion/fooutils/fooutils.d
@@ -4,6 +4,11 @@ struct Foo {
     void fooHey(){}
 }
 
+struct IntAliased {
+    int x;
+    alias x this;
+}
+
 void u(Foo foo) {}
 void ufcsHello(ref Foo foo) {}
 void ufcsBar(Foo foo, string mama) {}
@@ -13,4 +18,5 @@ void ufcsBarRefConstWrapped(ref const(Foo) foo, string mama) {}
 void ufcsBarRefImmuttableWrapped(ref immutable(Foo) foo, string mama) {}
 void ufcsBarScope(ref scope Foo foo, string mama) {}
 void ufcsBarReturnScope(return scope Foo foo, string mama) {}
+void ufcsSomeInt(int x) {}
 private void ufcsBarPrivate(Foo foo, string message) {}
