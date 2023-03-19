e711d80f5dfd1ce5e8a4f24a14aa84b518df62ca
WebFreak001 <gh@webfreak.org>
2023-03-19 23:22:09 +0100

    extended array & pointer tests

--- tests/tc_ufcs_array_type_completion/run.sh
+++ tests/tc_ufcs_array_type_completion/run.sh
@@ -1,5 +1,5 @@
 set -e
 set -u
 
-../../bin/dcd-client $1 -c65 file.d > actual_array_test.txt
+../../bin/dcd-client $1 -c259 file.d > actual_array_test.txt
 diff actual_array_test.txt expected_array_test.txt
