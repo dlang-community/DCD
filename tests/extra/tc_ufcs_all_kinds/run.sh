aa5b40434fe8a7dd185408d7927882af6342c7b6
Andrei Horodniceanu <a.horodniceanu@proton.me>
2025-07-01 22:02:50 +0300

    tests/extra/tc_ufcs_all_kinds: support gdc
    Change the format in which errors are printed to keep it consistent
    across all the three major compilers (a style that they all support is
    the gnu style) and make the matching code in generate_tests.d more
    readable by using a regex.
    
    Signed-off-by: Andrei Horodniceanu <a.horodniceanu@proton.me>

--- tests/extra/tc_ufcs_all_kinds/run.sh
+++ tests/extra/tc_ufcs_all_kinds/run.sh
@@ -4,4 +4,19 @@ if [ -z "${DC:-}" ]; then
 	DC=dmd
 fi
 
-DC="$DC" "$DC" -run generate_tests.d "$1"
+DCBASE=$(basename "${DC}")
+
+# Set up ERROR_STYLE to make all compilers output errors in the same
+# format to make matching easier in generate_tests.d.
+
+if [[ ${DCBASE} =~ gdmd ]]; then
+    ERROR_STYLE=
+elif [[ ${DCBASE} =~ dmd|ldc ]]; then
+    ERROR_STYLE='-verror-style=gnu -vcolumns'
+else
+    echo "unknonwn compiler ${DC}"
+    exit 1
+fi
+
+export DC ERROR_STYLE
+${DC} -run generate_tests.d "${1}"
