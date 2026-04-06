1309f2b3b07a33bffebe28482cedebb05f17c4f9
Dan Vu <danvu@imada.sdu.dk>
2026-04-06 15:09:02 +0200

    Fixing one off

--- dsymbol/src/dsymbol/ufcs.d
+++ dsymbol/src/dsymbol/ufcs.d
@@ -198,7 +198,7 @@ private const(Token)* findUFCSBaseToken(const(Token)[] tokens, out const(Token)[
         if (t is tok!"identifier")
         {
             // If this is a function call, then extract the arguments
-            if (i + 1 <= tokens.length && tokens[i + 1] == tok!"(" && tokens[$ - 1] == tok!")")
+            if (tokens.length >= 2 && i + 1 < tokens.length && tokens[i + 1] == tok!"(" && tokens[$ - 1] == tok!")")
             {
                 auto argTokens = tokens[i + 2 .. $ - 1]; // get whats inside ( ... )
                 foreach (argToken; argTokens)
