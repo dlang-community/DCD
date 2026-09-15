1fa08cb7dc6c3442671786821635570ddc3e60a3
Dan Vu <danvu@imada.sdu.dk>
2026-09-15 08:34:44 +0200

    wip

--- dsymbol/src/dsymbol/conversion/first.d
+++ dsymbol/src/dsymbol/conversion/first.d
@@ -1026,6 +1026,22 @@ final class FirstPass : ASTVisitor
 		ifs.accept(this);
 	}
 
+	override void visit(const Catch c) {
+		if (c.declarationOrStatement is null) {
+			c.accept(this);
+			return;
+		}
+		pushScope(c.declarationOrStatement.startLocation, c.declarationOrStatement.endLocation);
+		scope(exit) popScope();
+
+		auto symbol = allocateSemanticSymbol(c.identifier.text, CompletionKind.variableName, symbolFile: symbolFile, c.identifier.index);
+		addTypeToLookups(symbol.typeLookups, c.type);
+		currentSymbol.addChild(symbol, true);
+		currentScope.addSymbol(symbol.acSymbol, owns: true);
+		c.accept(this);
+	}
+
+
 	override void visit(const WithStatement withStatement)
 	{
 		if (withStatement.expression !is null
